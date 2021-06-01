f9151eae0a1a1d40fbf9a5b5c83e094f5c7abae7
Scarlet Rioseco <sriozeco@gmail.com>
2021-05-31 21:32:58 -0400

    suma frutas

---
+++ script.js
@@ -0,0 +1,9 @@
+var raul=[4, 0, 0];
+var pedro=[2, 0, 4];
+var alberto=[0, 8, 0];
+
+var manzana= raul[0]+pedro[0]+alberto[0];
+
+var pera= raul[1]+pedro[1]+alberto[1];
+
+var frutilla= raul[2]+pedro[2]+alberto[2];
