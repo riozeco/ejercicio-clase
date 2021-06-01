96ec4318e269b045e9dc5e93270c23726683d9fc
Scarlet Rioseco <sriozeco@gmail.com>
2021-05-31 21:41:27 -0400

    suma final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//[manzana, pera, frutilla]
+
 var raul=[4, 0, 0];
 var pedro=[2, 0, 4];
 var alberto=[0, 8, 0];
@@ -7,3 +9,7 @@ var manzana= raul[0]+pedro[0]+alberto[0];
 var pera= raul[1]+pedro[1]+alberto[1];
 
 var frutilla= raul[2]+pedro[2]+alberto[2];
+
+var frutillaCompartida = frutilla /2;
+
+var sumaFruta = manzana+pera+frutilla;
